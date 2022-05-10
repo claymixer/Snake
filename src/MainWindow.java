18a7856846a2ec8a9ef63925e701c0eaae2c87af
claymixer <belkirill9@gmail.com>
2022-05-10 23:57:42 +0300

    First commit

---
+++ src/MainWindow.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+
+    public MainWindow(){
+
+        setTitle("Snake Souls"); // название
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // возможность закрытия
+        setSize(800,825); // размер
+        setResizable(false); //запрет на измененние размеров
+        setLocation(320,0); // раположение фрейма
+        add(new GameField());
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {    MainWindow mainWindow = new MainWindow();   }
+}
